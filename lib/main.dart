4a30487ac9865fa4adab8038c8700a4c034fec62
amman1910 <amirmn@post.jce.ac.il>
2026-06-04 11:26:17 +0300

    feat: refactor MyApp to use StatefulWidget and initialize PushNotificationService

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; 
+import 'firebase_options.dart';
 // Import the welcome page
 import 'features/auth/screens/welcome_page.dart';
+import 'features/notifications/services/push_notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      PushNotificationService.instance.initialize();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: PushNotificationService.navigatorKey,
       debugShowCheckedModeBanner: false,
       title: 'Wurkit',
       theme: ThemeData(
@@ -30,7 +43,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // Set the initial page to WelcomePage
-      home: const WelcomePage(), 
+      home: const WelcomePage(),
     );
   }
 }
